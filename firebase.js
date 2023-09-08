bae0fa66a61e8ba987b1cfa13392386caae26426
Ala <alaaeddine.ayari1@esprit.tn>
2023-09-08 18:46:39 +0200

    chore:__

--- firebase.js
+++ firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.API_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
